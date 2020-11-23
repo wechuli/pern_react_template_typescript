e908d49dd9b3bb77ab379362ab09a780d588cb33
Wechuli <wechulipaul@yahoo.com>
2020-11-23 17:19:32 +0300

    typeorm and initial server files

---
+++ server/app.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import cors from 'cors';
+import compression from 'compression';
+import cookieSession from 'cookie-session';
+import helmet from 'helmet';
+
+
+
+// Middleware configuration
